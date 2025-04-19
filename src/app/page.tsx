409757ea2c8adce889b21f70e76a805c48aecd13
Rion <info@ri0n.dev>
2025-04-19 13:40:56 +0900

    Remove: <Confitti />

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Cards } from "@/components/section/cards";
 export default function Home() {
   return (
     <div className="flex md:items-center md:justify-center h-screen">
-      {/* <Confetti /> */}
       <div className="flex flex-col md:items-center md:justify-center w-full md:max-w-2xl gap-5 p-4">
         <Profile />
         <Cards />
