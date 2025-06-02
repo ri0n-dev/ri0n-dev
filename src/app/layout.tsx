ccba4089195a8b5e67ae4e4a44ae1831c3c9506e
Rion <info@ri0n.dev>
2025-06-02 21:59:05 +0900

    add: white background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export { metadata as metadata } from "@/consts/metadata";
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <body className="font-geist antialiased">
+      <body className="font-geist bg-white antialiased">
         {children}
       </body>
     </html>
