96de8f84e1307212eb2e1fd2fe64b616dc73ed2e
Rion <info@ri0n.dev>
2025-06-22 23:16:30 +0900

    fix: update dependencies and add missing autoprefixer to fix build error

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,19 @@
-module.exports = {
-    theme: {
-        extend: {
-            fontFamily: {
-                cormorant: ['var(--font-cormorant)', 'serif'],
-            },
-        },
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        cormorant: ['var(--font-cormorant)', 'serif'],
+      },
     },
+  },
+  plugins: [],
 };
+
+export default config;
