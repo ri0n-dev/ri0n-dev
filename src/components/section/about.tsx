4267fa8a88620008de772564b4b1c2f9d0ab35c8
Rion <info@ri0n.dev>
2025-06-25 00:49:02 +0900

    remove: pointer

--- src/components/section/about.tsx
+++ src/components/section/about.tsx
@@ -46,63 +46,59 @@ export default function About() {
                     </BentoCard>
 
                     <BentoCard className="col-span-2 row-span-4 relative p-6 flex flex-col">
-                        <FollowerPointerCard
-
-                        >
-                            <div className="relative flex flex-col h-full">
-                                <Image src="/assets/zisty.svg" alt="zisty" width={500} height={500} className="drop-shadow-[0_0_50px_rgba(255,255,255,0.7)] absolute top-1/2 left-1/2 w-full h-full pb-7 object-contain opacity-20 z-0 -translate-x-1/2 -translate-y-1/2" />
-                                <div className="flex-1 flex items-center justify-center mb-4 z-10">
-                                    <div className="*:data-[slot=avatar]:ring-neutral-800 flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1914457806679498756/gi2pV6jy_400x400.jpg" alt="@ri0n_dev" />
-                                            <AvatarFallback>RI</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1879463762627072000/WnD4sJ1V_400x400.jpg" alt="@tanahiro2010" />
-                                            <AvatarFallback>TH</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://cdn.discordapp.com/avatars/1004348660181630977/6445e3f6eddfbad4b0130e9680552673.webp?size=1024" alt="@kazu" />
-                                            <AvatarFallback>KZ</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1772492028636737536/bClftlxv_400x400.jpg" alt="@syobosyobonn" />
-                                            <AvatarFallback>SY</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://9z5q.github.io/lovelang.png" alt="@sigma" />
-                                            <AvatarFallback>SG</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1863135999087505408/5qjunahh_400x400.jpg" alt="@xxtomm" />
-                                            <AvatarFallback>TM</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1915770562779209728/yZyDjjpC_400x400.jpg" alt="@rai" />
-                                            <AvatarFallback>RA</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1873678657379774464/_IXeWAJ0_400x400.jpg" alt="@umaidango" />
-                                            <AvatarFallback>UM</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://cdn.discordapp.com/avatars/1381227903508811848/0cb68ff304e2db706f4bb98a60b9a64a.webp?size=512" alt="@yunai" />
-                                            <AvatarFallback>YN</AvatarFallback>
-                                        </Avatar>
-                                        <Avatar>
-                                            <AvatarImage src="https://pbs.twimg.com/profile_images/1773615356420239360/UglquU_j_400x400.jpg" alt="@takoyaki" />
-                                            <AvatarFallback>TK</AvatarFallback>
-                                        </Avatar>
-                                    </div>
+                        <div className="relative flex flex-col h-full">
+                            <Image src="/assets/zisty.svg" alt="zisty" width={500} height={500} className="drop-shadow-[0_0_50px_rgba(255,255,255,0.7)] absolute top-1/2 left-1/2 w-full h-full pb-7 object-contain opacity-20 z-0 -translate-x-1/2 -translate-y-1/2" />
+                            <div className="flex-1 flex items-center justify-center mb-4 z-10">
+                                <div className="*:data-[slot=avatar]:ring-neutral-800 flex -space-x-2 *:data-[slot=avatar]:ring-2 *:data-[slot=avatar]:grayscale">
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1914457806679498756/gi2pV6jy_400x400.jpg" alt="@ri0n_dev" />
+                                        <AvatarFallback>RI</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1879463762627072000/WnD4sJ1V_400x400.jpg" alt="@tanahiro2010" />
+                                        <AvatarFallback>TH</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://cdn.discordapp.com/avatars/1004348660181630977/6445e3f6eddfbad4b0130e9680552673.webp?size=1024" alt="@kazu" />
+                                        <AvatarFallback>KZ</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1772492028636737536/bClftlxv_400x400.jpg" alt="@syobosyobonn" />
+                                        <AvatarFallback>SY</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://9z5q.github.io/lovelang.png" alt="@sigma" />
+                                        <AvatarFallback>SG</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1863135999087505408/5qjunahh_400x400.jpg" alt="@xxtomm" />
+                                        <AvatarFallback>TM</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1915770562779209728/yZyDjjpC_400x400.jpg" alt="@rai" />
+                                        <AvatarFallback>RA</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1873678657379774464/_IXeWAJ0_400x400.jpg" alt="@umaidango" />
+                                        <AvatarFallback>UM</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://cdn.discordapp.com/avatars/1381227903508811848/0cb68ff304e2db706f4bb98a60b9a64a.webp?size=512" alt="@yunai" />
+                                        <AvatarFallback>YN</AvatarFallback>
+                                    </Avatar>
+                                    <Avatar>
+                                        <AvatarImage src="https://pbs.twimg.com/profile_images/1773615356420239360/UglquU_j_400x400.jpg" alt="@takoyaki" />
+                                        <AvatarFallback>TK</AvatarFallback>
+                                    </Avatar>
                                 </div>
                             </div>
-                            <div className="space-y-4">
-                                <h2 className="text-neutral-100 text-3xl md:text-4xl font-bold">Team</h2>
-                                <p className="text-neutral-400 text-sm md:text-base leading-relaxed font-geist">
-                                    It&apos;s a team mainly composed of student developers. I founded the team and serve as its owner.
-                                </p>
-                            </div>
-                        </FollowerPointerCard>
+                        </div>
+                        <div className="space-y-4">
+                            <h2 className="text-neutral-100 text-3xl md:text-4xl font-bold">Team</h2>
+                            <p className="text-neutral-400 text-sm md:text-base leading-relaxed font-geist">
+                                It&apos;s a team mainly composed of student developers. I founded the team and serve as its owner.
+                            </p>
+                        </div>
                     </BentoCard>
                 </div>
 
