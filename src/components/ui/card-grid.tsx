a9dd1c1379759c9796116660e90bcd351f93fe1d
Rion <info@ri0n.dev>
2025-08-31 01:19:07 +0900

    add: fix h

--- src/components/ui/card-grid.tsx
+++ src/components/ui/card-grid.tsx
@@ -20,7 +20,7 @@ export function CardGrid() {
                             <Team />
                         </div>
                     </div>
-                    <div className="relative h-65 mt-4 pb-5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
+                    <div className="relative h-60 mt-4 pb-5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
                         <div className="absolute top-2 left-2 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Favorite Songs</div>
                         <Songs />
                     </div>
@@ -53,23 +53,23 @@ export function CardGrid() {
             </div >
 
             {/* Desktop */}
-            < div className="hidden md:grid grid-cols-3 gap-4 mt-10 max-h-50" >
-                <BlurFade delay={0.8}>
+            <div className="hidden md:grid grid-cols-3 gap-4 mt-10 max-h-56" >
+                <BlurFade delay={0.8} className="max-h-56">
                     <div className="relative pt-11.5 h-full border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden">
                         <div className="absolute top-2 left-2 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Favorite Langs</div>
                         <Lang />
                     </div>
                 </BlurFade>
 
-                <BlurFade delay={0.9}>
-                    <div className="relative pb-3 h-full border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
+                <BlurFade delay={0.9} className="max-h-56">
+                    <div className="relative pt-2 pb-5 h-full border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
                         <div className="absolute top-2 left-2 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Favorite Songs</div>
                         <Songs />
                     </div>
                 </BlurFade>
 
-                <BlurFade delay={1}>
-                    <div className="relative pb-3 h-full border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-[#f6f6f6] dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
+                <BlurFade delay={1} className="max-h-56">
+                    <div className="relative pt-2 pb-5 h-full border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-[#f6f6f6] dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
                         <div className="absolute top-2 left-2 z-10 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Affiliated Team</div>
                         <Team />
                     </div>
