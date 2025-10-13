be55ccc20f79c3684347ad96ea1974634d3c1f1d
Rion <info@ri0n.dev>
2025-10-14 00:05:32 +0900

    fix

--- src/components/ui/card-grid.tsx
+++ src/components/ui/card-grid.tsx
@@ -8,18 +8,22 @@ export function CardGrid() {
         <>
             {/* Phone */}
             <div className="phone-hidden">
-                <BlurFade delay={1.2}>
-                    <div className="grid grid-cols-3 gap-4 mt-10">
-                        <div className="relative grid grid-cols-2 h-50 pt-11.5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden col-span-2">
+                <div className="grid grid-cols-2 gap-4 mt-10">
+                    <BlurFade delay={1.2}>
+                        <div className="relative h-50 pt-11.5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden">
                             <div className="absolute top-2 left-2 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Favorite Langs</div>
                             <Lang />
                         </div>
+                    </BlurFade>
 
-                        <div className="relative grid grid-cols-1 h-50 pt-2 pb-5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-[#f6f6f6] dark:bg-neutral-950 overflow-hidden">
+                    <BlurFade delay={1.3}>
+                        <div className="relative h-50 pt-2 pb-5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-[#f6f6f6] dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
                             <div className="absolute top-2 left-2 z-10 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Affiliated Team</div>
                             <Team />
                         </div>
-                    </div>
+                    </BlurFade>
+                </div>
+                <BlurFade delay={1.4}>
                     <div className="relative h-60 mt-4 pb-5 border-2 border-neutral-200 dark:border-neutral-800/80 rounded-xl bg-neutral-50 dark:bg-neutral-950 overflow-hidden flex items-center justify-center">
                         <div className="absolute top-2 left-2 text-[10px] bg-neutral-100 dark:bg-neutral-50/5 rounded-lg px-2 py-1 text-neutral-500 dark:text-neutral-300">Favorite Songs</div>
                         <Songs />
