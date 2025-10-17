79af9dc4136d18bf6b7cc081fa57b0646628e18d
Rion <info@ri0n.dev>
2025-10-17 23:35:36 +0900

    fix

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -7,7 +7,7 @@ interface TimelineItemProps {
 const TimelineItem: React.FC<TimelineItemProps> = ({ date, title, description }) => {
   return (
     <div className="relative flex items-start">
-      <div className="absolute left-[7.5px] top-0 w-2 h-2 bg-neutral-50 shadow rounded-full mt-1.5 z-10"></div>
+      <div className="absolute left-[7.5px] top-0 w-2 h-2 dark:bg-neutral-50 bg-neutral-600 shadow rounded-full mt-1.5 z-10"></div>
       <div className="ml-8">
         <p className="text-[10px] text-neutral-500">{date}</p>
         <h3 className="text-sm font-semibold">{title}</h3>
@@ -24,7 +24,7 @@ interface TimelineProps {
 const Timeline: React.FC<TimelineProps> = ({ items }) => {
   return (
     <div className="relative mx-auto max-w-md h-screen">
-      <div className="absolute left-2.5 top-0 bottom-0 w-[3px] bg-neutral-900 z-5 mt-1.5"></div>
+      <div className="absolute left-2.5 top-0 bottom-0 w-[3px] dark:bg-neutral-900 bg-neutral-200 z-5 mt-1.5"></div>
       <div className="space-y-6">
         {items.map((item, index) => (
           <TimelineItem
