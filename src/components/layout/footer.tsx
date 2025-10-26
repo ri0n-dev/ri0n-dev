f2d3b6ef01d4b5d59fad28c95ca37a1f1b2a457a
Rion <info@ri0n.dev>
2025-10-26 13:29:54 +0900

    fix

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export function Footer() {
     return (
-        <footer className="flex items-center justify-between w-full mb-4 mt-20 text-center text-sm text-neutral-600">
+        <footer className="flex items-center justify-between w-full mb-4 mt-20 text-center text-xs md:text-sm text-neutral-600">
             <p className="text-left">© {new Date().getFullYear()}</p>
             <div className="flex gap-3">
                 <Link href="/home" className="hover:underline">/home</Link>
