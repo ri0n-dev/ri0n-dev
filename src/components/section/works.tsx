ee1483e4a1a6ce0f6b596ead4bdcb9c4536017b0
Rion <info@ri0n.dev>
2025-11-09 01:00:45 +0900

    add

--- src/components/section/works.tsx
+++ src/components/section/works.tsx
@@ -32,7 +32,7 @@ export function Works() {
                     delay={1.6}
                     title="111-theme"
                     href="https://github.com/ri0n-dev/111-theme"
-                    description="This is a theme based on #111, and it works with VSCode. Used by over 300 people."
+                    description="This is a theme based on #111, and it works with VSCode. Downloaded by over 300 times."
                     imageSrcs={[{ src: "/works/111-theme/1.png", alt: "111-theme - 1" }]}
                 />
 
